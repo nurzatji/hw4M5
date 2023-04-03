049e5fbaacba7821860f2f17fd810de054151377
Nurzat <taalajbekovanurzat98@gmail.com>
2023-04-03 19:10:53 +0600

    щщ

--- app/src/main/java/com/geektech/lovecalculator/CalculatorFragment.kt
+++ app/src/main/java/com/geektech/lovecalculator/CalculatorFragment.kt
@@ -23,7 +23,7 @@ class CalculatorFragment : Fragment() {
 
 
     private lateinit var binding: FragmentCalculatorBinding
-val viewModel:LoveViewModel by viewModels()
+    val viewModel: LoveViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,31 +42,35 @@ val viewModel:LoveViewModel by viewModels()
     private fun initListener() {
         with(binding) {
             btnCalculate.setOnClickListener {
-                viewModel.qetLiveLoveModel(etFirstName.text.toString(), etSecondName.text.toString()).observe(this@CalculatorFragment,
-                    Observer {
-                        Log.e("ololo","initClickers: $it")
-                    })
-                LoveService().api.getPercentage(
+                viewModel.qetLiveLoveModel(
                     etFirstName.text.toString(),
                     etSecondName.text.toString()
-                ).enqueue(object : Callback<LoveModel> {
-                    override fun onResponse(call: Call<LoveModel>, response: Response<LoveModel>) {
-                        if (response.isSuccessful) {
-                            Log.e("ololo", "onResponse: ${response.body()}")
-                            findNavController().navigate(
-                                R.id.resultFragment,
-                                bundleOf("result" to response.body())
-                            )
-                            etFirstName.text.clear()
-                            etSecondName.text.clear()
-                        }
-                    }
+                ).observe(this@CalculatorFragment,
+                    Observer {
+                        Log.e("ololo", "initClickers: $it")
+                    })
+//              //  LoveService().api.getPercentage(
+//                    etFirstName.text.toString(),
+//                    etSecondName.text.toString()
+//                ).enqueue(object : Callback<LoveModel> {
+//                    override fun onResponse(call: Call<LoveModel>, response: Response<LoveModel>) {
+//                        if (response.isSuccessful) {
+//                            Log.e("ololo", "onResponse: ${response.body()}")
+//                findNavController().navigate(
+//                    R.id.resultFragment
 
-                    override fun onFailure(call: Call<LoveModel>, t: Throwable) {
-                        Log.e("ololo", "onResponse: ${t.message}")
-                    }
-                })
+
             }
+
+//                            etFirstName.text.clear()
+//                            etSecondName.text.clear()
+//                        }
+//                    }
+//
+//                    override fun onFailure(call: Call<LoveModel>, t: Throwable) {
+//                        Log.e("ololo", "onResponse: ${t.message}")
+//                    }
+
         }
     }
 }
